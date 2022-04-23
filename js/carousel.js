ec0d2f9d5ca004a0e4f75842b8a79968d99c1f8d
victorcova <victor.avoc@gmail.com>
2022-04-23 19:36:56 -0300

    carousel-responsivo-ok

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,27 @@
-jQuery('.slider').slick({  
+// slider SLICK responsivo
+
+$('.slider').slick({
   dots: true,
+  infinite: false,
+  speed: 300,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }        
+  ]
 });
