750166a604bb25d8e84484400c0e1ff6924d1706
victorcova <victor.avoc@gmail.com>
2022-04-24 00:20:57 -0300

    pgs-entrar+cadastrar

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,15 @@
 function buscarCurso(){
     alert('Buscar Cursos...');
 }
+
+function efetuarLogin(){
+    alert('Login de usuário...');
+}
+
+function pgEntrar() {    
+    window.open('entrar.html', '_blank');
+}
+
+function pgCadastrar() {    
+    window.open('cadastro.html', '_blank');
+}
